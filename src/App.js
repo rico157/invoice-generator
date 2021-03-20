2396df9da8a93a24a70f7433168e5a3f35d8663a
Riccardo Cogoni <cogonir@live.com>
2021-03-20 23:09:20 +0000

    update name / remove button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import Invoice from "./components/Invoice/Invoice";
 import Navbar from "./components/Navbar";
-import printDocument from "./utils/pdfUtils";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Invoice />
-      <button onClick={printDocument} style={{ width: "3cm", height: "3cm" }}>
-        Download
-      </button>
     </div>
   );
 }
